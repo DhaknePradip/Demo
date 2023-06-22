9a776af6ba1d6ef6c48b1eca5051d0eb901bba10
PradipDhakne <dhaknepradip17@gmail.com>
2023-06-22 18:04:51 +0530

    TEST

---
+++ practice/src/test/class1.java
@@ -0,0 +1,15 @@
+package test;
+
+import demo.base;
+
+public class class1 extends base {
+	class2 class2= new class2 ();
+public void example1() 
+{
+	System.out.println("class 2");
+}
+public static void main(String[] args) {
+	
+
+}
+}
